b83de318d67ee4ead6a5b2179cd9ede16d2410eb
lppduy <lephamphuongduy@gmail.com>
2023-11-12 12:08:25 +0700

    [12/11/23] init project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/******
+ * TITLE: AXE GAME CPP
+ * DEV: LPPDUY
+ * CREATED: 12/11/2023
+ * UPDATED: 12/11/2023
+******/
+
+#include <cstdio>
+
+int main()
+{
+  // print out my name
+  printf("Hi, im lppduy\n");
+  // print my city
+  printf("I'm from sg\n");
+  // my manifestion
+  printf("I will be the king of the world\n");
+}
